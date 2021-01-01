6231e96d97817e6b678636e05a6cd1dcc5d8f663
Ardahan Kisbet <ardahankisbet@gmail.com>
2021-01-01 16:07:12 +0300

    enum helper class has been added

---
+++ CSharpUtility/Helpers/EnumHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpUtility.Helpers
+{
+    public class EnumHelper
+    {
+        /// <summary>
+        /// Extension method for Generic Enum Conversion
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="param"></param>
+        /// <param name="ignoreCase"></param>
+        /// <returns></returns>
+        public static T ParseEnum<T>(string param, bool ignoreCase = true)
+        {
+            return (T)System.Enum.Parse(typeof(T), param, ignoreCase);
+        }
+    }
+}
