294fdcc30df95fa293615db94194d72bc5df0a02
Ardahan Kisbet <ardahankisbet@gmail.com>
2021-01-05 18:44:17 +0300

    Working on helper functions. Doors Helper class has been added.

--- CSharpUtility/Helpers/DirectoryHelper.cs
+++ CSharpUtility/Helpers/DirectoryHelper.cs
@@ -6,11 +6,13 @@ namespace CSharpUtility.Helpers
 {
     class DirectoryHelper
     {
+        /// <summary>To get all files with a given extension under given root folder</summary>
         public List<FileInfo> GetFiles(string path, string pattern = "*.*")
         {
             return new DirectoryInfo(path).GetFiles(pattern, SearchOption.AllDirectories).ToList();
         }
 
+        /// <summary>To get a collection of files from given root array with a given extension</summary>
         public List<FileInfo> GetFiles(string[] path, string pattern)
         {
             List<FileInfo> temp = new List<FileInfo>();
---
+++ CSharpUtility/Helpers/DoorsDXLHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpUtility.Helpers
+{
+    class DoorsDXLHelper
+    {
+        /// <summary>
+        /// Run given dxl script on Doors COM Interface
+        /// </summary>
+        /// <param name="query"></param>
+        public void Eval(string query)
+        {
+            try
+            {
+                DoorsDXLClass obj = new DoorsDXLClass();
+                // Use obj.result to communicate with dxl results
+                // oleSetResult, oleGetResult
+
+                obj.runStr("print \"Hello\" \"\n\"; oleSetResult \"Yes\"");
+                obj.runStr("print \"\n\"");
+                obj.runStr(query);
+            }
+            catch (System.Exception e) { }
+        }
+
+    }
+}
